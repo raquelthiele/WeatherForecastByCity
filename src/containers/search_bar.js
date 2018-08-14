6753f95560b2c6bc190a9f9d1b618566a694db65
raquelthiele <raquelthiele@hotmail.com>
2018-08-14 16:33:53 -0300

    Linked the search bar input to the state

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -2,10 +2,30 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({ term: event.target.value });
+  }
+
   render() {
     return (
       <form className="input-group">
-        <input />
+        <input
+          placeholder="Cidade que deseja saber a previsão do tempo para os próximos  dias"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
+        <span className="input-group-btn">
+          <button type="submit" className="btn  btn-secondary">Pesquisar</button>
+        </span>
       </form>
     );
   }
