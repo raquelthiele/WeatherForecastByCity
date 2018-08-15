3796598bbb0b5718db3a53445d7832d4bb3f3c6c
raquelthiele <raquelthiele@hotmail.com>
2018-08-15 12:37:52 -0300

    SetUp the url to search the forecast by city

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,13 @@
 const API_KEY = 'ecca5023ce870865264719944c1356be';
+const ROOT_URL  = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER =  'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+
+  return {
+    type: FETCH_WEATHER,
+
+  };
+}
