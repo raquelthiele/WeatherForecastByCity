895a2a154ecfacb53cadc1f75c4e3a65a40f40fa
raquelthiele <raquelthiele@hotmail.com>
2018-08-16 11:15:50 -0300

    Added Google Maps to cities

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 
 class GoogleMap extends Component {
   componentDidMount() {
-    new google.map.Map(this.refs.map, {
-      zoom: 12,
+    new google.maps.Map(this.refs.map, {
+      zoom: 10,
       center: {
         lat: this.props.lat,
         lng: this.props.lon
